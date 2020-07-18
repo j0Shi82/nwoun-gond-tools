f5c8c2d308175a38ecf7e97e22c5594df4088b92
j0Shi82 <janosch.ferda@e-domizil.de>
2020-07-19 00:21:23 +0200

    baby steps with basic design

--- src/App.svelte
+++ src/App.svelte
@@ -6,17 +6,22 @@ import backgroundImage from 'assets/media/images/NW_M14_Artwork.jpg';
 import headerBanner from 'assets/media/images/nwunlogo.png';
 
 import 'assets/style/tailwind.scss';
+import 'assets/style/global.scss';
 
 setupLocalization();
 </script>
 
 <style lang="scss">
   #background {
-    @apply w-full h-auto min-h-full fixed top-0 left-0 z-0 opacity-25
+    @apply w-full h-auto min-h-full fixed top-0 left-0 z-0 opacity-25 object-left object-cover;
   }
 
   #main-content {
-    @apply relative z-10 p-2
+    @apply relative z-10 mt-0 mr-auto mb-0 ml-auto max-w-screen-xl;
+  }
+
+  #sticky {
+    box-shadow: 0 5px 20px 0 rgba(0,0,0,.75);
   }
 
 </style>
@@ -24,9 +29,11 @@ setupLocalization();
 {#if !$isLocalizationLoading}
 <img src="{backgroundImage}" id="background">
 <main class="main-content" id="main-content">
-    <div id="header" class="flex justify-center lg:justify-start">
+    <div id="header" class="flex flex-col lg:flex-row justify-center items-center lg:justify-start p-2">
       <img src="{headerBanner}" id="headerBanner" class="h-16 md:h-32" />
+      <div id="subtitle" class="text-center w-full text-sm md:text-lg lg:text-2xl">Neverwinteer's #1 independent news source and community from 2015 - 2019!</div>
     </div>
+    <div id="sticky" class="sticky top-0 w-full bg-red-700 h-12 z-20"></div>
     <RouterComponent {routes} on:routeLoaded="{routerOnRouteLoaded}" />
 </main>
 {/if}
