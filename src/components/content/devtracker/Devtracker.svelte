68880b54280a333da81d68b10867d67e17a98fb6
j0Shi82 <janosch.ferda@e-domizil.de>
2020-07-29 02:04:27 +0200

    update packages

--- src/components/content/devtracker/Devtracker.svelte
+++ src/components/content/devtracker/Devtracker.svelte
@@ -7,13 +7,13 @@ let error = false;
 // const avatarData = {};
 
 svelteLifecycleOnMount(() => {
-  axios.get('https://api.uncnso.red/v1/devtracker')
+  axios.get('https://api.uncnso.red/v1/devtracker/list')
     .then((response) => {
       apiData = response.data;
 
       apiData.forEach((data) => {
         axios({
-          url: `https://forum.arcgames.com/neverwinter/api/v1/users/get.json?User.Name=${data.dev_name}`,
+          url: `https://api.uncnso.red/v1/devtracker/devinfo?dev=${data.dev_name}`,
         }).then((res) => {
           console.log(res);
         });
