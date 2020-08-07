40426dea841c149aeeeca9a9e446cba11c01612a
j0Shi82 <janosch.ferda@e-domizil.de>
2020-08-07 16:30:31 +0200

    start working on infohub

---
+++ src/components/content/infohub/Infohub.svelte
@@ -0,0 +1,25 @@
+<script>
+import { axios } from 'utils/imports/core';
+import { svelteLifecycleOnMount } from 'utils/imports/svelte';
+
+let pcData = {};
+
+function getPCPosts() {
+  axios.get('https://api.uncnso.red/v1/nwfeeds/arcgamespc').then((response) => {
+    pcData = response.data;
+  });
+}
+
+svelteLifecycleOnMount(() => {
+  getPCPosts();
+});
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+{#each pcData as data}
+    <div class="w-full bg-nwoun p-2">
+        <a href="{data.link}" target="_blank">{data.title}</a>
+    </div>
+{/each}
+</div>
+
