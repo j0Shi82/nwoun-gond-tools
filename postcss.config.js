2ee81d3dfc5e0d23ac156648d8f8d41c03dae5a6
j0Shi82 <janosch.ferda@e-domizil.de>
2022-09-12 02:13:25 +0200

    ditched purgecss since tailwind 3 now uses a different method internally and it led to purge unintentionally killing all css

--- postcss.config.js
+++ postcss.config.js
@@ -2,25 +2,10 @@ const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 const postcssDiscardDuplicates = require('postcss-discard-duplicates');
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/**/*.html',
-    './src/**/*.svelte',
-  ],
-
-  whitelistPatterns: [/svelte-/],
-
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
-
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
-
 module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer(),
-    ...(prod ? [purgecss] : []),
     postcssDiscardDuplicates,
   ],
 };
