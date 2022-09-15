b9194ba50b9099b132b297e99f969c2b9720611d
j0Shi82 <janosch.ferda@e-domizil.de>
2022-09-15 11:48:56 +0200

    correct patreon links

--- src/components/content/auction/AuctionDataEngineInfo.svelte
+++ src/components/content/auction/AuctionDataEngineInfo.svelte
@@ -10,25 +10,25 @@ import { makeApiCall } from 'utils/imports/helpers';
 let crawlEngineData = null;
 
 $: localizeOptions = {
-    values: {
-        itemsPerDay: parseInt(crawlEngineData?.ItemsPerDay), 
-        totalItems: parseInt(crawlEngineData?.TotalItems), 
-        percentage: (parseInt(crawlEngineData?.ItemsPerDay)/parseInt(crawlEngineData?.TotalItems)*100).toFixed(2) 
-    }
-}
+  values: {
+    itemsPerDay: parseInt(crawlEngineData?.ItemsPerDay),
+    totalItems: parseInt(crawlEngineData?.TotalItems),
+    percentage: (parseInt(crawlEngineData?.ItemsPerDay) / parseInt(crawlEngineData?.TotalItems) * 100).toFixed(2),
+  },
+};
 
 svelteLifecycleOnMount(async () => {
-  crawlEngineData = await makeApiCall({ 
-    type: 'auctions/engine', 
-    returnData: true
+  crawlEngineData = await makeApiCall({
+    type: 'auctions/engine',
+    returnData: true,
   });
 });
 
 const classes = {
-    div1: "p-2 border-2 rounded-lg",
-    div2: "flex items-center justify-center",
-    div3: "text-center font-bold text-lg text-nwoun underline"
-}
+  div1: 'p-2 border-2 rounded-lg',
+  div2: 'flex items-center justify-center',
+  div3: 'text-center font-bold text-lg text-nwoun underline',
+};
 </script>
 
 {#if crawlEngineData}
@@ -50,7 +50,7 @@ const classes = {
         <div>{@html $localize('auction.engine.down.data', localizeOptions)}</div>
     </div>
     <div class="{classes.div3}">
-        <a href="https://www.patreon.com/nwoun" target="_blank">{ $localize('auction.engine.down.help') }</a>
+        <a href="https://www.patreon.com/nw_gond_tools" target="_blank">{ $localize('auction.engine.down.help') }</a>
     </div>
 </div>
 {/if}
@@ -59,7 +59,7 @@ const classes = {
 <div class="{classes.div1} border-gray-700 mb-2">
     <div class="text-center">{ $localize('auction.engine.loading.data') }</div>
     <div class="{classes.div3}">
-        <a href="https://www.patreon.com/nwoun" target="_blank">{ $localize('auction.engine.loading.help') }</a>
+        <a href="https://www.patreon.com/nw_gond_tools" target="_blank">{ $localize('auction.engine.loading.help') }</a>
     </div>
 </div>
 {/if}
